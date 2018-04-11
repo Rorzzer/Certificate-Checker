eb74a0040ae001b52e2e7fa8b0b060d2291e1236
RoryPowell <rpowell@student.unimelb.edu.au>
2018-04-11 17:15:13 +1000

    Parsing request, sending valid http reply.

--- server.c
+++ server.c
@@ -6,8 +6,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <errno.h>
+#include <unistd.h>
 
-#define PORT 8080
+#define PORT 1234
 
 void test_func(int sock);
 
@@ -35,7 +37,7 @@ int main()
     // Bind to the socket
     if(bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(struct sockaddr_in) ) < 0)
     {
-        printf("Error binding socket\n");
+        perror("Error binding socket\n");
         return -1;
     }
 
@@ -47,16 +49,74 @@ int main()
     // accept the incoming connection request
     int clientlen = sizeof(client_addr);
     newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, (socklen_t *)&clientlen);
+    printf("accepted new client\n");
 
     // do something with connection
     test_func(newsockfd);
+
+    close(newsockfd);
+    close(sockfd);
 
 }
 
 void test_func(int sock){
 
-    char buf[1000];
-    strcpy(buf, "connection test success!\n");
+    const int NOT_AUTH = 0;
 
-    send(sock, buf, strlen(buf), 0);
+    char sendbuf[10000];
+    char recbuf[1000];
+    char *token;
+    char *method;
+    char *path;
+    char *httpver;
+    const char methoddelim[2] = " ";
+    const char pathdelim[2] = " ";
+
+
+    strcpy(sendbuf, "HTTP/1.0 200 OK\r\n\r\n");
+    send(sock, sendbuf, strlen(sendbuf), 0);
+
+    int state = NOT_AUTH;
+
+//    while(1) {
+        // wait for client response
+        recv(sock, recbuf, 1000, 0);
+//        printf("%s\n", recbuf);
+        method = strtok(recbuf, methoddelim);
+//        printf("method: %s\n", method);
+
+        if (!strcmp(method, "GET")){
+            // get request
+            printf("Method: %s\n", method);
+            path = strtok(NULL, pathdelim);
+            printf("Path: %s\n", path);
+
+        } else if (!strcmp(method, "POST")){
+            // if a post request is sent
+            printf("This server currently only implements HTTP/1.0\n");
+        } else {
+            printf("Unrecognised HTTP request\n");
+        }
+//
+//        while(1){
+//            recv(sock, recbuf, 1000, 0);
+//            if (!strcmp(method, "GET")){
+//                test_func(sock);
+//            }
+//        }
+
+//    while( token != NULL ) {
+//        printf( " %s\n", token);
+//        token = strtok(NULL, s);
+//    }
+
+//        printf("%s\n", buf);
+//
+//        if (strncmp("GET", buf, 5)){
+//            printf("get request\n");
+//            strcpy(buf, "");
+//            send(sock, buf, strlen(buf), 0);
+//        }
+//    }
 }
+
